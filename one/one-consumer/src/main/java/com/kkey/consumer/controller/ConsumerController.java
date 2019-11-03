30cb65a681385685261388bd44d99b677a83a232
kkey <liufeibo91@163.com>
2019-11-03 23:00:46 +0800

    搭建两个版本 one：纯restTemplate two：集成eureka

---
+++ one/one-consumer/src/main/java/com/kkey/consumer/controller/ConsumerController.java
@@ -0,0 +1,25 @@
+package com.kkey.consumer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("consumer")
+public class ConsumerController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        String url = "http://localhost:6666/hello/" + name;
+        String result = restTemplate.getForObject(url, String.class);
+        return result + "  ==>> one consumer";
+    }
+
+}
---
+++ one/one-provider/src/main/java/com/kkey/provider/controller/ProviderController.java
@@ -0,0 +1,15 @@
+package com.kkey.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderController {
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        return name + ", hello world! ==>> provider";
+    }
+
+}
