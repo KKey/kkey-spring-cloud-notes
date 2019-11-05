ff90af325e10a0a179e527dcf5e66c2d259fdc3b
liufeibo <liufeibowj69406@touna.cn>
2019-11-05 18:06:45 +0800

    集成hystrix dashboard

--- five/five-consumer/src/main/java/com/kkey/consumer/controller/ConsumerFiveController.java
+++ five/five-consumer/src/main/java/com/kkey/consumer/controller/ConsumerFiveController.java
@@ -1,6 +1,7 @@
 package com.kkey.consumer.controller;
 
 import com.kkey.consumer.interfaces.ProviderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class ConsumerFiveController {
      * NOTE five：借助feign集成Hystrix
      */
     @GetMapping("/hello/{name}")
+    @HystrixCommand
     public String sayHelloRibbon(@PathVariable("name") String name) {
         return providerService.providerSayHello(name) + "  ==>> five consumer";
     }
--- five/five-provider-two/src/main/java/com/kkey/provider/controller/ProviderTwoController.java
+++ five/five-provider-two/src/main/java/com/kkey/provider/controller/ProviderTwoController.java
@@ -1,5 +1,6 @@
 package com.kkey.provider.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProviderTwoController {
 
     @GetMapping("/hello/{name}")
+    @HystrixCommand
     public String sayHello(@PathVariable("name") String name) {
         return name + ", hello world! ==>> five provider two";
     }
